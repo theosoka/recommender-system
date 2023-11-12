1bf52189dd31310b9866de5bce4022fdd570b058
Polina <lemann676@gmail.com>
2023-11-12 17:58:01 +0100

    RS-12 add data-processing script

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,9 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+from sklearn.preprocessing import QuantileTransformer
+import numpy as np
 
 
 @click.command()
@@ -14,6 +17,34 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    logger.info('reading raw datasets')
+
+    user_artists = pd.read_csv(Path(input_filepath) / "user_artists.dat", sep="\t")
+    logger.info('removing skewness in user_artists.weight')
+    qt = QuantileTransformer(n_quantiles=500, output_distribution='normal')
+    user_artists.weight = pd.Series((qt.fit_transform(np.array(user_artists.weight).reshape(-1, 1))).flatten())
+
+    user_friends = pd.read_csv(Path(input_filepath) / "user_friends.dat", sep="\t")
+
+    user_taggedartists = pd.read_csv(Path(input_filepath) / "user_taggedartists-timestamps.dat", sep="\t")
+
+    tags = pd.read_csv(Path(input_filepath) / "tags.dat", sep="\t", encoding='latin-1')
+
+    artists = pd.read_csv(Path(input_filepath) / "artists.dat", sep="\t", encoding='latin-1')
+    logger.info('dropping rows with null values')
+    artists = artists.dropna()
+
+    processed_dataframes = {
+        "user_artists": user_artists,
+        "user_friends": user_friends,
+        "user_taggedartists": user_taggedartists,
+        "tags": tags,
+        "artists": artists,
+    }
+
+    for name, df in processed_dataframes.items():
+        filename = f"{name}.csv"
+        df.to_csv(Path(output_filepath) / filename, index=False)
 
 
 if __name__ == '__main__':
