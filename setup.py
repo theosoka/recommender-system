b2a7ade1386c60bb3b02e8cb10fe9a7c56c489fd
Polina <lemann676@gmail.com>
2024-01-21 15:06:29 +0100

    get recommendations and general updates

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='practical part for my bachelor thesis at PJATK, using data from spotify',
-    author='Polina Lysenko',
-    license='MIT',
+    version="0.1.0",
+    description="practical part for my bachelor thesis at PJATK, using data from lastfm",
+    author="Polina Lysenko",
+    license="MIT",
 )
--- src/models/collaborative_filtering.py
+++ src/models/collaborative_filtering.py
@@ -11,19 +11,20 @@ from scipy.sparse import csr_matrix
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import LabelEncoder
 
-from train_model import ModelMixin
+from .train_model import ModelMixin
 
 
 @dataclass
 class CollaborativeFiltering(ModelMixin):
     user_artist_matrix: Optional[csr_matrix] = None
+    artist_user_matrix: Optional[csr_matrix] = None
     artist_similarity: Optional[cosine_similarity] = None
     user_label_encoder: Optional = None
     artist_label_encoder: Optional = None
 
     @property
     def user_ids(self):
-        pass
+        return set(self.dataset.userID)
 
     @property
     def artists_ids(self):
@@ -35,6 +36,7 @@ class CollaborativeFiltering(ModelMixin):
 
     def get_item_item_similarity(self, user_ids, artist_ids):
         artist_user_matrix = csr_matrix(([1] * len(user_ids), (artist_ids, user_ids)))
+        self.artist_user_matrix = artist_user_matrix
 
         similarity = cosine_similarity(artist_user_matrix)
         return similarity, artist_user_matrix
@@ -44,100 +46,78 @@ class CollaborativeFiltering(ModelMixin):
     ):
         user_artist_matrix = csr_matrix(artist_user_matrix.T)
         user_artist_scores = user_artist_matrix.dot(similarity_matrix)
+
         rec_for_crust = []
         for user_id in range(user_artist_scores.shape[0]):
             scores = user_artist_scores[user_id, :]
-        purchased_items = user_artist_matrix.indices[
-            user_artist_matrix.indptr[user_id] : user_artist_matrix.indptr[user_id + 1]
-        ]
-        scores[purchased_items] = -1  # do not recommend already purchased SalesItems
-        top_products_ids = np.argsort(scores)[-top_n:][::-1]
-        recommendations = pd.DataFrame(
-            top_products_ids.reshape(1, -1),
-            index=[user_id],
-            columns=["Top%s" % (i + 1) for i in range(top_n)],
-        )
-        rec_for_crust.append(recommendations)
+            purchased_items = user_artist_matrix.indices[
+                user_artist_matrix.indptr[user_id] : user_artist_matrix.indptr[
+                    user_id + 1
+                ]
+            ]
+            scores[purchased_items] = -1
+            top_products_ids = np.argsort(scores)[-top_n:][::-1]
+            recommendations = pd.DataFrame(
+                top_products_ids.reshape(1, -1),
+                index=[user_id],
+                columns=["Top%s" % (i + 1) for i in range(top_n)],
+            )
+            rec_for_crust.append(recommendations)
+
         return pd.concat(rec_for_crust)
 
-    def fit(self, data):
-        self.dataset = data
-        self.user_label_encoder = LabelEncoder()
-        self.artist_label_encoder = LabelEncoder()
+    def get_recommendations(self, data):
+        user_label_encoder = LabelEncoder()
 
-        user_ids = self.user_label_encoder.fit_transform(data.userID)
-        artist_ids = self.artist_label_encoder.fit_transform(data.artistID)
-
-        self.user_artist_matrix = csr_matrix(
-            ([1] * len(user_ids), (artist_ids, user_ids))
+        user_ids = user_label_encoder.fit_transform(data.userID)
+        product_label_encoder = LabelEncoder()
+        product_ids = product_label_encoder.fit_transform(data.artistID)
+        similarity_matrix, artist_user_sim_matrix = self.get_item_item_similarity(
+            user_ids, product_ids
         )
-
-        self.artist_similarity = cosine_similarity(self.user_artist_matrix)
-
-    def get_recommendations_for_new_user(self, favorite_artists: list[str], top_n=10):
-        if self.user_artist_matrix is None or self.artist_similarity is None:
-            raise ValueError("Model not fitted. Please call fit() method first.")
-
-        # Transform favorite artists to encoded IDs
-        favorite_artist_ids = self.artist_label_encoder.transform(favorite_artists)
-
-        # Create a user vector for the new user
-        new_user_vector = csr_matrix(
-            (
-                [1] * len(favorite_artist_ids),
-                (favorite_artist_ids, [0] * len(favorite_artist_ids)),
+        recommendations = self.get_recommendations_from_similarity(
+            similarity_matrix, artist_user_sim_matrix
+        )
+        recommendations.index = user_label_encoder.inverse_transform(
+            recommendations.index
+        )
+        for i in range(recommendations.shape[1]):
+            recommendations.iloc[:, i] = product_label_encoder.inverse_transform(
+                recommendations.iloc[:, i]
             )
-        )
-
-        # Extend the new user vector to include all artists in the original matrix
-        all_artist_ids = range(self.user_artist_matrix.shape[0])
-        new_user_vector_extended = csr_matrix(
-            ([1] * len(all_artist_ids), (all_artist_ids, [0] * len(all_artist_ids)))
-        )
-
-        # Calculate similarity between the new user and existing artists
-        similarity_with_existing_artists = cosine_similarity(
-            self.user_artist_matrix.T, new_user_vector_extended.T
-        )
-
-        print("User Artist Matrix Shape:", self.user_artist_matrix.shape)
-        print("Similarity Matrix Shape:", similarity_with_existing_artists.shape)
-
-        # Calculate recommendations for the new user
-        scores = (
-            self.user_artist_matrix.dot(similarity_with_existing_artists.T)
-            .todense()
-            .A.flatten()
-        )
-        top_products_ids = np.argsort(scores)[-top_n:][::-1]
-
-        # Convert back to artist names
-        recommendations = self.artist_label_encoder.inverse_transform(top_products_ids)
-
         return recommendations
 
 
-dataset = pd.read_csv(Path() / "../../data/processed/lastfm_2k/user_artists.csv")
-
-model = CollaborativeFiltering(model_name="Collaborative Filtering", dataset=dataset)
-model.fit(
-    dataset
-)  # You need to fit the model with the existing dataset before making recommendations
-artists = [
-    "Moonspell",
-    "Marilyn Manson",
-    "MALICE MIZER",
-    "Diary of Dreams",
-    "Tamtrum",
-    "Amduscia",
-    "Covenant",
-    "KMFDM",
-    "Emperor",
-    "Morcheeba",
-]
-artists_df = pd.read_csv(Path() / "../../data/processed/lastfm_2k/artists.csv")
-
-ids = artists_df[artists_df.name.isin(artists)].id
-
-recommendations = model.get_recommendations_for_new_user(list(ids))
-print(recommendations)
+# dataset = pd.read_csv(Path() / "../../data/processed/lastfm_2k/user_artists.csv")
+#
+# artists = [
+#     "Moonspell",
+#     "Marilyn Manson",
+#     "MALICE MIZER",
+#     "Diary of Dreams",
+#     "Tamtrum",
+#     "Amduscia",
+#     "Covenant",
+#     "KMFDM",
+#     "Emperor",
+#     "Morcheeba",
+# ]
+# artists_df = pd.read_csv(Path() / "../../data/processed/lastfm_2k/artists.csv")
+#
+# ids = artists_df[artists_df.name.isin(artists)].id
+# print(dataset.shape)
+# new_data = pd.DataFrame({"userID": [1] * 10, "artistID": list(ids), "weight": [1] * 10})
+# dataset = pd.concat([dataset, new_data], ignore_index=True)
+# print(list(ids))
+#
+#
+# model = CollaborativeFiltering(model_name="Collaborative Filtering", dataset=dataset)
+#
+# # Get recommendations for the new user
+# # new_user_recommendations = model.get_recommendations(dataset)
+# #
+# # print("Recommendations for the new user:")
+# # new_recs = new_user_recommendations.iloc[0]
+# # print(list(new_recs))
+#
+# pickle.dump(model, open(Path() / "../../models/cf.pkl", "wb"))
