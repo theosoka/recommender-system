12956844d9153f5bacdbf9bd532a8a5567fb41cf
Polina <lemann676@gmail.com>
2024-02-04 19:32:33 +0100

    read data from s3

--- pages/01_📊_Data_Exploration.py
+++ pages/01_📊_Data_Exploration.py
@@ -2,9 +2,15 @@ import pandas as pd
 import ydata_profiling
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
+from st_files_connection import FilesConnection
+from last_fm_api.make_data_from_s3 import get_dataframes
 
 st.set_page_config(page_title="Data Exploration", page_icon="📊")
 st.title("Datasets Profiles")
+
+conn = st.connection("s3", type=FilesConnection)
+s3_folder_path = "plysenko-thesis-datasets/data/hetrec2011-lastfm-2k/"
+file_list = conn.list(s3_folder_path)
 
 datasets_names = [
     "artists",
@@ -15,39 +21,24 @@ datasets_names = [
 ]
 
 artists, tags, user_artists, user_friends, user_tagged_artists = st.tabs(datasets_names)
-
+artists_df, tags_df, user_artists_df, user_friends_df, user_tagged_artists_df = get_dataframes()
 
 with artists:
-    artists_df = pd.read_csv(
-        "/Users/polina/study/THESIS/recommender-system/data/processed/lastfm_2k/artists.csv"
-    )
     pr = artists_df.profile_report()
     st_profile_report(pr)
 
 with tags:
-    tags_df = pd.read_csv(
-        "/Users/polina/study/THESIS/recommender-system/data/processed/lastfm_2k/tags.csv"
-    )
     pr_tags = tags_df.profile_report()
     st_profile_report(pr_tags)
 
 with user_artists:
-    user_artists_df = pd.read_csv(
-        "/Users/polina/study/THESIS/recommender-system/data/processed/lastfm_2k/user_artists_prepared.csv"
-    )
     pr_user_artists = user_artists_df.profile_report()
     st_profile_report(pr_user_artists)
 
 with user_friends:
-    user_friends_df = pd.read_csv(
-        "/Users/polina/study/THESIS/recommender-system/data/processed/lastfm_2k/user_friends.csv"
-    )
     pr_user_friends = user_friends_df.profile_report()
     st_profile_report(pr_user_friends)
 
 with user_tagged_artists:
-    user_tagged_artists_df = pd.read_csv(
-        "/Users/polina/study/THESIS/recommender-system/data/processed/lastfm_2k/user_tagged_artists.csv"
-    )
     pr_user_tagged_artists = user_tagged_artists_df.profile_report()
     st_profile_report(pr_user_tagged_artists)
